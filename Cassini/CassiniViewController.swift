7add6da516b649c70d510337ee7b3caeeb0c474a
Gustavo Ordaz <ordazsgustavo@gmail.com>
2018-12-05 09:03:49 -0500

    Lesson 11 part 1

---
+++ Cassini/CassiniViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CassiniViewController.swift
+//  Cassini
+//
+//  Created by Gustavo Ordaz on 12/2/18.
+//  Copyright © 2018 Gustavo Ordaz. All rights reserved.
+//
+
+import UIKit
+
+class CassiniViewController: UIViewController {
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier {
+            if let url = DemoURLs.NASA[identifier] {
+                if let imageVC = segue.destination.contents as? ImageViewController {
+                    imageVC.imageURL = url
+                    imageVC.title = (sender as? UIButton)?.currentTitle
+                }
+            }
+        }
+    }
+
+}
+
+extension UIViewController {
+    var contents: UIViewController {
+        if let navcon = self as? UINavigationController {
+            return navcon.visibleViewController ?? self
+        } else {
+            return self
+        }
+    }
+}
